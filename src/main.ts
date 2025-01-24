6d734e696d8c440819530f6304c47dd7199cbc51
Matin <mt662270@dal.ca>
2025-01-24 19:12:08 -0400

    asset folder structured

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { Application, Assets, Sprite } from "pixi.js";
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
   // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const texture = await Assets.load("/assets/characters/bunny.png");
 
   // Create a bunny Sprite
   const bunny = new Sprite(texture);
