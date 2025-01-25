29b22848b6af9da141c073397d2ebb2f3e8aa60c
peterbetts <peter.douglas.betts@gmail.com>
2025-01-25 13:27:30 -0400

    added more js for testing

--- src/ui.js
+++ src/ui.js
@@ -4,6 +4,14 @@ window.addEventListener("DOMContentLoaded", () => {
     
     document.getElementById("connect-button").addEventListener("click", function() {
 
+        // get values from input fields
+        let username = document.getElementById("username-input").value;
+        let server = document.getElementById("server-input").value;
+
+        console.log(server);
+        console.log(username);
+
+        // hide #connection-panel and show #connecting
         document.getElementById("connection-panel").style.display = "none";
         document.getElementById("connecting").style.display = "flex";
 
@@ -11,6 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("main-menu-button").addEventListener("click", function() {
 
+        // hide #death-container and show #main-menu-container
         document.getElementById("death-container").style.display = "none";
         document.getElementById("main-menu-container").style.display = "flex";
 
@@ -18,5 +27,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// once connected, display:none on #connection-container and display:flex on #app
+
+// to do:
+// once connected, display:none on #main-menu-container and display:flex on #app
 // on death, set #app to display:none and set #death-container to display:flex
