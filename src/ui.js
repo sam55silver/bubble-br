cf82cdbd1af48ded3118de6a1056f5da83b9b572
peterbetts <peter.douglas.betts@gmail.com>
2025-01-25 12:54:50 -0400

    added death screen, new font, refined previous elements

--- src/ui.js
+++ src/ui.js
@@ -1,12 +1,22 @@
+// just basics to get the buttons working
+
 window.addEventListener("DOMContentLoaded", () => {
-    let target = document.getElementById("connect-button");
     
-    target.addEventListener("click", function() {
+    document.getElementById("connect-button").addEventListener("click", function() {
 
         document.getElementById("connection-panel").style.display = "none";
         document.getElementById("connecting").style.display = "flex";
 
     });
+
+    document.getElementById("main-menu-button").addEventListener("click", function() {
+
+        document.getElementById("death-container").style.display = "none";
+        document.getElementById("main-menu-container").style.display = "flex";
+
+    });
 });
 
-// just basics to get the button working
+
+// once connected, display:none on #connection-container and display:flex on #app
+// on death, set #app to display:none and set #death-container to display:flex
